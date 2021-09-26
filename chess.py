b270795911816adec58b6a8e2d088125fd0acd64
jesse berkowitz <jberkow@brandeis.edu>
2021-09-26 14:58:03 -0400

    great start

--- chess.py
+++ chess.py
@@ -35,18 +35,20 @@ def load_images():
         IMAGES[piece]= p.transform.scale(p.image.load(piece+'.png'), (Sq_Size, Sq_Size))
 
 
-# def drawPieces(screen, gs):
-
-
-# def draw_gamestate(screen, gs):
-#     drawBoard(screen)
-#     drawPieces(screen, gs)
 def drawBoard(screen):
     colors = [p.Color('white'), p.Color('gray')]
     for r in range(Dimensions):
         for c in range(Dimensions):
             color = colors[((r+c)%2)]
             p.draw.rect(screen, color, p.Rect(c*Sq_Size, r*Sq_Size, Sq_Size, Sq_Size))
+def draw_pieces(screen, board):
+    
+    for r in range(Dimensions):
+        for c in range(Dimensions):
+            piece = board[r][c]
+            if piece != '-':
+                screen.blit(IMAGES[piece], p.Rect(c*Sq_Size, r*Sq_Size, Sq_Size, Sq_Size))
+
 def main():
     screen = p.display.set_mode((Width, Height))
     clock = p.time.Clock()
@@ -58,8 +60,9 @@ def main():
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
-        drawBoard(screen)        
-        # draw_gamestate(screen, gs)
+        drawBoard(screen)
+        draw_pieces(screen, gs.board)        
+        
         clock.tick(Max_FPS)
         p.display.flip()         
 
