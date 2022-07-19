a8d33ca6f72b6fc256b1234c3a5274872e663e87
jesse berkowitz <jberkow@brandeis.edu>
2022-07-19 08:25:34 -0700

    pieces drawn

--- Chess_v2.py
+++ Chess_v2.py
@@ -14,12 +14,14 @@ class Game:
     def __init__(self, size):
         self.size = size
         self.pieces = ['kn', 'r', 'b', 'q', 'k', 'p']
+        self.screen = p.display.set_mode((Width, Height))   
         self.board = self.create_board()
-        self.screen = p.display.set_mode((Width, Height))
         self.Sq_sz = int(Width/self.size)
         self.IMAGES = {}
-    
+        self.load_images()
+       
     def create_board(self):
+        
         Board = []
         top_row = []
         bottom_row = []
@@ -41,20 +43,29 @@ class Game:
         Board.append(black_panws)
         Board.append(bottom_row)
         return Board
-    
-    def drawBoard(self):
-        colors = [p.Color('white'), p.Color('gray')]
-        for r in range(self.size):
-            for c in range(self.size):
-                color = colors[((r+c)%2)]
-                p.draw.rect(self.screen, color, p.Rect(c*self.Sq_sz, r*self.Sq_sz, self.Sq_sz, self.Sq_sz))
-
+        
     def load_images(self):
 
         pieces = ['br', 'bkn', 'bb', 'bq', 'bk', 'bp', 'wr', 'wkn', 'wb', 'wq', 'wk', 'wp']
         for piece in pieces:
             self.IMAGES[piece]= p.transform.scale(p.image.load(piece+'.png'), (self.Sq_sz, self.Sq_sz))
+    
+    def drawBoard(self):
 
+        colors = [p.Color('white'), p.Color('gray')]
+        for r in range(self.size):
+            for c in range(self.size):
+                color = colors[((r+c)%2)]
+                p.draw.rect(self.screen, color, p.Rect(c*self.Sq_sz, r*self.Sq_sz, self.Sq_sz, self.Sq_sz))
+        return self.screen
+
+    def draw_pieces(self):
+    
+        for r in range(self.size):
+            for c in range(self.size):
+                piece = self.board[r][c]
+                if piece != '-':
+                    self.screen.blit(self.IMAGES[piece], p.Rect(c*self.Sq_sz, r*self.Sq_sz, self.Sq_sz, self.Sq_sz))
 
 
 def main():
@@ -69,6 +80,8 @@ def main():
     
         G.screen.fill(p.Color('white'))
         G.drawBoard()
+        G.draw_pieces()
+
         clock.tick(Max_FPS)
         p.display.flip()   
 
